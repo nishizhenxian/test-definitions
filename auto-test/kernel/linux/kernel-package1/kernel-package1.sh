d71e082b7067e9c36a08e2a50cd78a201584520e
nishizhenxian <1599075576@qq.com>
2018-11-15 18:13:28 +0800

    update kernel-package1

--- auto-test/kernel/linux/kernel-package1/kernel-package1.sh
+++ auto-test/kernel/linux/kernel-package1/kernel-package1.sh
@@ -18,67 +18,68 @@ cd -
 ######################  testing the step #############################
 case "${distro}" in
     debian)
-		source_version_remove(){    
-			p=$1
-			s=$(apt show $p | grep "Source" | awk '{print $2}')	             
-			v=$(apt show $p | grep "Version" | awk '{print $2}')		              
-			if [ "$s" = "${source1}" -o "$s" = "${source2}" ];then				                 
-			   print_info 0 ${p}_source
-			else
-			   print_info 1 ${p}_source
-			fi
+	source_version_remove(){    
+		p=$1
+		s=$(apt show $p | grep "Source" | awk '{print $2}')	             
+		v=$(apt show $p | grep "Version" | awk '{print $2}')		              
+		if [ "$s" = "${source1}" -o "$s" = "${source2}" ];then				                 
+		   print_info 0 ${p}_source
+		else
+		   print_info 1 ${p}_source
+		fi
 
-			if [ "$v" = "${iversion}" -o "$v" = "${version2}" -o "$v" = "${version3}" ];then
-				print_info 0 ${p}_version
-			else
-				print_info 1 ${p}_version
-			fi
+		if [ "$v" = "${iversion}" -o "$v" = "${version2}" -o "$v" = "${version3}" ];then
+			print_info 0 ${p}_version
+		else
+			print_info 1 ${p}_version
+		fi
 
-			uname=$(uname -r)
-			if [ "$p" = "linux-image-${uname}" ];then
-			echo -e "$blue this package isn't remove$NC"
+		uname=$(uname -r)
+		if [ "$p" = "linux-image-${uname}" ];then
+		echo -e "$blue this package isn't remove$NC"
+		else
+		   apt remove -y $p
+		   print_info $? ${p}_remove
+		fi
+	}
+	sed -i s/5.[0-9]/5.2/g /etc/apt/sources.list.d/estuary.list
+	apt-get update
+	apt-get install -y libcpupower-dev linux-estuary-doc usbip > /dev/null
+	iversion=$(apt show libcpupower-dev| grep "Version" | awk '{print $2}')
+	version2=$(apt show linux-estuary-doc| grep "Version" | awk '{print $2}')
+	version3=$(apt show usbip| grep "Version" | awk '{print $2}')
+	source1=$(apt show libcpupower-dev| grep "Source"|awk '{print $2}')
+	source2=$(apt show linux-estuary-doc| grep "Source"|awk '{print $2}')
+	apt-get remove -y libcpupower-dev linux-estuary-doc usbip > /dev/null
+	package_list="libcpupower1 libcpupower-dev linux-cpupower linux-estuary-doc linux-estuary-perf linux-estuary-source linux-headers linux-headers-estuary-arm64 linux-image linux-image-estuary-arm64 linux-kbuild linux-libc-dev linux-perf linux-source linux-support usbip "
+	for p in ${package_list};do
+		echo "$p install................."
+		apt-get install -y $p
+		vs=$(apt show $p | grep "Version" | awk '{print $2}')
+		status=$?
+		if [ "$vs" != "$iversion" -o "$vs" != "$version2" -o "$vs" != "$version3" ];then
+		   status=1 
+		fi
+		if [ $status -eq 0 ];then
+			print_info 0 ${p}_install
+			source_version_remove $p
+		else
+			pa=$(apt search $p | grep $iversion | grep -v db | grep $p |cut -d "/" -f 1)
+			pa_num=$(apt search $p | grep $iversion | grep -v db | grep $p |cut -d "/" -f 1|wc -l)
+			if [ $pa_num = 1 ]; then
+				apt-get install -y $pa
+			print_info 0 ${pa}_install
+			source_version_remove $pa
 			else
-			   apt remove -y $p
-			   print_info $? ${p}_remove
+				for i in $pa ;do
+					apt-get install -y $i
+				print_info 0 ${i}_install
+			source_version_remove $i
+				done
 			fi
-		}
-		sed -i s/5.[0-9]/5.2/g /etc/apt/sources.list.d/estuary.list
-		apt-get update
-		apt-get install -y libcpupower-dev linux-estuary-doc usbip > /dev/null
-		iversion=$(apt show libcpupower-dev| grep "Version" | awk '{print $2}')
-		version2=$(apt show linux-estuary-doc| grep "Version" | awk '{print $2}')
-		version3=$(apt show usbip| grep "Version" | awk '{print $2}')
-		source1=$(apt show libcpupower-dev| grep "Source"|awk '{print $2}')
-		source2=$(apt show linux-estuary-doc| grep "Source"|awk '{print $2}')
-		apt-get remove -y libcpupower-dev linux-estuary-doc usbip > /dev/null
-		package_list="libcpupower1 libcpupower-dev linux-cpupower linux-estuary-doc linux-estuary-perf linux-estuary-source linux-headers linux-headers-estuary-arm64 linux-image linux-image-estuary-arm64 linux-kbuild linux-libc-dev linux-perf linux-source linux-support usbip "
-		for p in ${package_list};do
-			echo "$p install................."
-			apt-get install -y $p
-			vs=$(apt show $p | grep "Version" | awk '{print $2}')
-			status=$?
-			if [ "$vs" != "$iversion" -o "$vs" != "$version2" -o "$vs" != "$version3" ];then
-			   status=1 
-			fi
-			if [ $status -eq 0 ];then
-				print_info 0 ${p}_install
-				source_version_remove $p
-			else
-				pa=$(apt search $p | grep $iversion | grep -v db | grep $p |cut -d "/" -f 1)
-				pa_num=$(apt search $p | grep $iversion | grep -v db | grep $p |cut -d "/" -f 1|wc -l)
-				if [ $pa_num = 1 ]; then
-					apt-get install -y $pa
-				print_info 0 ${pa}_install
-				source_version_remove $pa
-				else
-					for i in $pa ;do
-						apt-get install -y $i
-					print_info 0 ${i}_install
-				source_version_remove $i
-					done
-				fi
-			fi
-		done
+		fi
+	done
+	;;
     centos)
         sed -i s/5.[0-9]/5.2/g /etc/yum.repos.d/estuary.repo
         yum clean all
