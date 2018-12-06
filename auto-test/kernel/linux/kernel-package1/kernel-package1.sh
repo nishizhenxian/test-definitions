0dac68c6d1b73ed75f11798ddccff145a9815e40
nishizhenxian <1599075576@qq.com>
2018-12-06 19:40:11 +0800

    update kernel-package

--- auto-test/kernel/linux/kernel-package1/kernel-package1.sh
+++ auto-test/kernel/linux/kernel-package1/kernel-package1.sh
@@ -20,10 +20,10 @@ case "${distro}" in
     debian)
 	install_source_version_remove(){    
 		p=$1
+		apt-get install -y $p
+        print_info $? ${p}_install
 		s=$(apt show $p | grep "Source" | awk '{print $2}')	             
 		v=$(apt show $p | grep "Version" | awk '{print $2}')
-        apt-get install -y $p
-        print_info $? ${p}_install
 		
 		if [ "$s" = "${source1}" -o "$s" = "${source2}" ];then				                 
 		   print_info 0 ${p}_source
@@ -45,8 +45,7 @@ case "${distro}" in
 		   print_info $? ${p}_remove
 		fi
 	}
-#	sed -i s/5.[0-9]/5.2/g /etc/apt/sources.list.d/estuary.list
-#	apt-get update
+
 	apt-get install -y libcpupower-dev linux-estuary-doc usbip > /dev/null
 	iversion=$(apt show libcpupower-dev| grep "Version" | awk '{print $2}')
 	version2=$(apt show linux-estuary-doc| grep "Version" | awk '{print $2}')
@@ -56,7 +55,7 @@ case "${distro}" in
 	apt-get remove -y libcpupower-dev linux-estuary-doc usbip > /dev/null
 	package_list="libcpupower1 libcpupower-dev linux-cpupower linux-estuary-doc linux-estuary-perf linux-estuary-source linux-headers linux-headers-estuary-arm64 linux-image linux-image-estuary-arm64 linux-kbuild linux-libc-dev linux-perf linux-source linux-support usbip "
 #	package_list="linux-perf linux-source "
-#package_list="libcpupower1 linux-estuary-doc linux-estuary-perf linux-estuary-source linux-headers-estuary-arm64 linux-image-estuary-arm64"
+#   package_list="libcpupower1 linux-estuary-doc linux-estuary-perf linux-estuary-source linux-headers-estuary-arm64 linux-image-estuary-arm64"
 	for p in ${package_list};do
 		echo "$p install................."
 		apt-get install -y $p
@@ -65,7 +64,6 @@ case "${distro}" in
 		if [ "$vs" != "$iversion" -a "$vs" != "$version2" -a "$vs" != "$version3" ];then
 			status=1
 		fi
-
 
 		if [ $status -eq 0 ];then
 			install_source_version_remove $p
@@ -76,82 +74,55 @@ case "${distro}" in
 			    install_source_version_remove $pa
 			else
 				for i in $pa ;do
-				        install_source_version_remove $i
+				    install_source_version_remove $i
 				done
 			fi
 		fi
 	done
 	;;
     centos)
-        sed -i s/5.[0-9]/5.2/g /etc/yum.repos.d/estuary.repo
-        yum clean all
-	yum install -y kernel-tools-libs-devel >/dev/null
+        install_from_repo_source_version_remove(){
+			p=$1
+			yum install -y $p > /dev/null
+			print_info $? ${p}_install
+			v=$(yum info $p | grep -i "version" | awk '{print $3}')
+			r=$(yum info $p | grep -i "release" | awk '{print $3}')
+			f=$(yum info $p | grep -i "from repo" | awk '{print $4}')
+
+			if [ "$f" = "$from_repo" ];then
+				print_info 0 ${p}_from_repo
+			else
+				print_info 1 ${p}_from_repo
+			fi
+
+			if [ "$r" = "${release}" ];then
+			   print_info 0 ${p}_source
+			else
+			   print_info 1 ${p}_source
+			fi
+
+			if [ "$v" = "${version}" ];then
+				print_info 0 ${p}_version
+			else
+				print_info 1 ${p}_version
+			fi
+
+			yum remove -y $p
+			print_info $? ${p}_remove > /dev/null
+        }
+	    yum install -y kernel-tools-libs-devel >/dev/null
         version=$(yum info kernel-tools-libs-devel | grep -i "version" | awk '{print $3}')
         release=$(yum info kernel-tools-libs-devel | grep -i "release" | awk '{print $3}')
         from_repo=$(yum info kernel-tools-libs-devel | grep -i "from repo" | awk '{print $4}')
         yum remove -y kernel-tools-libs-devel > /dev/null
-	#version="4.16.0"
-        #release="estuary.6"
-        #from_repo="Estuary"
+
         package_list="kernel-devel kernel-headers kernel-tools-libs kernel-tools-libs-devel perf python-perf  kernel-debug kernel-debug-debuginfo"
-        for p in ${package_list};do
-            echo "$p install"
-            yum install -y $p
-            status=$?
-            rmflag=0
-            if test $status -eq 0
-            then
-                 print_info 0 install
-                from=$(yum info $p | grep "From repo" | awk '{print $4}')
-                if [ "$from" = "$from_repo" ];then
-                   print_info 0 repo_check
-                else
-                    #已经安装，但是安装源不是estuary的情况需要卸载重新安装
-                    rmflag=1
-                    if [ "$from" != "Estuary" ];then
-                        yum remove -y $p
-                        yum install -y $p
-                        from=$(yum info $p | grep "From repo" | awk '{print $4}')
-                        if [ "$from" = "$from_repo" ];then
-                             print_info 0 repo_check
-                        else
-                            print_info 1 repo_check
-                        fi
-                    fi
-                fi
+        for pa in ${package_list};do
+            echo "$pa install............."
+            install_from_repo_source_version_remove $pa
+	    done
+    ;;
 
-                vs=$(yum info $p | grep "Version" | awk '{print $3}')
-                if [ "$vs" = "$version" ];then
-                      print_info 0 version
-                else
-                      print_info 1 version
-                fi
-
-                rs=$(yum info $p | grep "Release" | awk '{print $3}')
-                if [ "$rs" = "$release" ];then
-                     print_info 0 release
-                else
-                     print_info 1 release
-                fi
-                #对于自带的包不去做卸载处理
-                if test $rmflag -eq 0
-                then
-                    yum remove -y $p
-                    status=$?
-                    if test $status -eq 0
-                    then
-                        print_info 0 remove
-                    else
-                        print_info 1 remove
-                    fi
-                else
-                    echo "$p don't remove" | tee -a ${RESULT_FILE}
-                fi
-            else
-                echo "$p install [FAIL]"  | tee -a ${RESULT_FILE}
-            fi
-        done
-        ;;
     ubuntu)
         sed -i s/5.[0-9]/5.1/g /etc/apt/sources.list.d/estuary.list 
         apt-get update -q=2
--- auto-test/kernel/linux/kernel-package2/kernel-package2.sh
+++ auto-test/kernel/linux/kernel-package2/kernel-package2.sh
@@ -68,79 +68,48 @@ case "${distro}" in
         done
         ;;
     centos)
-        sed -i s/5.[0-9]/5.2/g /etc/yum.repos.d/estuary.repo
-		yum clean all
-        yum install -y kernel-tools-debuginfo >/dev/null
+        install_from_repo_source_version_remove(){
+			p=$1
+			yum install -y $p > /dev/null
+			print_info $? ${p}_install
+			v=$(yum info $p | grep -i "version" | awk '{print $3}')
+			r=$(yum info $p | grep -i "release" | awk '{print $3}')
+			f=$(yum info $p | grep -i "from repo" | awk '{print $4}')
+
+			if [ "$f" = "$from_repo" ];then
+				print_info 0 ${p}_from_repo
+			else
+				print_info 1 ${p}_from_repo
+			fi
+
+			if [ "$r" = "${release}" ];then
+			   print_info 0 ${p}_source
+			else
+			   print_info 1 ${p}_source
+			fi
+
+			if [ "$v" = "${version}" ];then
+				print_info 0 ${p}_version
+			else
+				print_info 1 ${p}_version
+			fi
+
+			yum remove -y $p
+			print_info $? ${p}_remove > /dev/null
+        }
+		yum install -y kernel-tools-debuginfo > /dev/null
         version=$(yum info kernel-tools-debuginfo | grep -i "version" | awk '{print $3}')
         release=$(yum info kernel-tools-debuginfo | grep -i "release" | awk '{print $3}')
         from_repo=$(yum info kernel-tools-debuginfo | grep -i "from repo" | awk '{print $4}')
         yum remove -y kernel-tools-debuginfo > /dev/null
-        #version="4.18.14"
-        #release="estuary.9"
-        #from_repo="Estuary"
         package_list="kernel-debug-devel kernel-debuginfo kernel-debuginfo-common-aarch64 kernel-tools-debuginfo perf-debuginfo python-perf-debuginfo"
-        for p in ${package_list};do
-            echo "$p install"
-            yum install -y $p
-            status=$?
-            rmflag=0
-            if test $status -eq 0
-            then
-                 print_info 0 install
-                from=$(yum info $p | grep "From repo" | awk '{print $4}')
-                from1=$(yum info $p | grep "From repo" | awk '{print $4}'|sed -n '2p')
-                if [ "$from" = "$from_repo" -o  "$from1" = "$from_repo" ];then
-                   print_info 0 repo_check
-                else
-                    #已经安装，但是安装源不是estuary的情况需要卸载重新安装
-                    rmflag=1
-                    if [ "$from" != "Estuary" ];then
-                        yum remove -y $p
-                        yum install -y $p
-                        from=$(yum info $p | grep "From repo" | awk '{print $4}')
-                        from1=$(yum info $p | grep "From repo" | awk '{print $4}'|sed -n '2p')
-                        if [ "$from" = "$from_repo" -o "$from1" = "$from_repo" ];then
-                             print_info 0 repo_check
-                        else
-                            print_info 1 repo_check
-                        fi
-                    fi
-                fi
-
-                vs=$(yum info $p | grep "Version" | awk '{print $3}')
-                vs1=$(yum info $p | grep "Version" | awk '{print $3}'|sed -n '2p')
-                if [ "$vs" = "$version" -o "$vs1" = "$version" ];then
-                      print_info 0 version
-                else
-                      print_info 1 version
-                fi
-
-                rs=$(yum info $p | grep "Release" | awk '{print $3}')
-                rs1=$(yum info $p | grep "Release" | awk '{print $3}'|sed -n '2p')
-                if [ "$rs" = "$release" -o "$rs1" = "$release" ];then
-                     print_info 0 release
-                else
-                     print_info 1 release
-                fi
-                #对于自带的包不去做卸载处理
-                if test $rmflag -eq 0
-                then
-                    yum remove -y $p
-                    status=$?
-                    if test $status -eq 0
-                    then
-                        print_info 0 remove
-                    else
-                        print_info 1 remove
-                    fi
-                else
-                    echo "$p don't remove" | tee -a ${RESULT_FILE}
-                fi
-            else
-                echo "$p install [FAIL]"  | tee -a ${RESULT_FILE}
-            fi
+        
+		for pa in ${package_list};do
+            echo "$pa install............."
+            install_from_repo_source_version_remove $pa
         done
-        ;;
+    ;;
+
     ubuntu)
         sed -i s/5.[0-9]/5.1/g /etc/apt/sources.list.d/estuary.list
         apt-get update -q=2
